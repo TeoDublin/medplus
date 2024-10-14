b75a66e10f8e311e184e13a8c13f57c8e29b94a2
teo <teodublin@gmail.com>
2024-10-15 01:29:27 +0200

    bug template

--- post/menu.php
+++ post/menu.php
@@ -1,3 +1,9 @@
 <?php 
     require '../includes.php';
-var_dump($_POST);
+    if($set['u_user'])$set['u_user']=$_POST['user'];
+    if($set['u_pasword'])$set['u_pasword']=$_POST['password'];
+    if($set['u_template'])$set['u_template']=$_POST['template'];
+    Users()->update(params: ['set'=>$set,'u_id'=>Session()->get('user_id')]);
+    $user=users()->first(['u_id'=>Session()->get('user_id')]);
+    Session()->start($user);
+    redirect($_SERVER['HTTP_REFERER']);
