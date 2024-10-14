69c593af566b95c774134b9f165b28ab1f90e264
teo <teodublin@gmail.com>
2024-10-15 01:16:25 +0200

    test hook

--- unity.php
+++ unity.php
@@ -1,6 +1,7 @@
 <?php
     require 'includes.php';
     require 'header.php';
+    echo "test";
     var_dump(users()->first(['u_id'=>1]));
     var_dump(error_get_last());
     require 'footer.php';
