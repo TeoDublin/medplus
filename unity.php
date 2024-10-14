d0a4f143bec8d8e01ca59e5fc64f11d0943fecf2
teo <teodublin@gmail.com>
2024-10-15 01:19:21 +0200

    test

--- unity.php
+++ unity.php
@@ -1,7 +1,7 @@
 <?php
     require 'includes.php';
     require 'header.php';
-    echo "test2";
+    echo "test3";
     var_dump(users()->first(['u_id'=>1]));
     var_dump(error_get_last());
     require 'footer.php';
