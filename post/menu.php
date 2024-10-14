b10ad7f2aaabccbc8e63b210cb37a15cec9c2393
teo <teodublin@gmail.com>
2024-10-15 01:30:25 +0200

    post

--- post/menu.php
+++ post/menu.php
@@ -1,8 +1,8 @@
 <?php 
     require '../includes.php';
-    if($set['u_user'])$set['u_user']=$_POST['user'];
-    if($set['u_pasword'])$set['u_pasword']=$_POST['password'];
-    if($set['u_template'])$set['u_template']=$_POST['template'];
+    if($_POST['u_user'])$set['u_user']=$_POST['user'];
+    if($_POST['u_pasword'])$set['u_pasword']=$_POST['password'];
+    if($_POST['u_template'])$set['u_template']=$_POST['template'];
     Users()->update(params: ['set'=>$set,'u_id'=>Session()->get('user_id')]);
     $user=users()->first(['u_id'=>Session()->get('user_id')]);
     Session()->start($user);
