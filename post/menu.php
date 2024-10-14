b3c12e21aac567c26c7bb8f5546d7f298c9a9450
teo <teodublin@gmail.com>
2024-10-15 01:34:51 +0200

    fix

--- post/menu.php
+++ post/menu.php
@@ -1,8 +1,9 @@
 <?php 
     require '../includes.php';
-    if($_POST['u_user'])$set['u_user']=$_POST['user'];
-    if($_POST['u_pasword'])$set['u_pasword']=$_POST['password'];
-    if($_POST['u_template'])$set['u_template']=$_POST['template'];
+    $set=[];
+    if(!empty($_POST['user']))$set['u_user']=$_POST['user'];
+    if(!empty($_POST['pasword']))$set['u_pasword']=$_POST['password'];
+    if(!empty($_POST['template']))$set['u_template']=$_POST['template'];
     Users()->update(params: ['set'=>$set,'u_id'=>Session()->get('user_id')]);
     $user=users()->first(['u_id'=>Session()->get('user_id')]);
     Session()->start($user);
