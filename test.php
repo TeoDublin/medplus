e58a5e47ff36211ac4ba89223e27b7170fac9609
teo <teodublin@gmail.com>
2024-10-26 17:48:57 +0200

    hour-piker1

--- test.php
+++ test.php
@@ -1,6 +1,5 @@
 <?php 
 require 'includes.php';
 require 'includes/header.php';
-component('calendar',['id'=>'test','container-class'=>'start-0 top-0','toggleElement'=>'.test']);
-component('calendar',['id'=>'test2','container-class'=>'start-50 top-0','toggleElement'=>'.test']);
+component('hour-picker',[]);
 require 'includes/footer.php';
