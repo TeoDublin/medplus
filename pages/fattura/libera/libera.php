21a7d3d52e089db35ae71b613890f199048f3f98
= <=>
2024-11-14 22:14:47 +0100

    fattura

--- pages/fattura/libera/libera.php
+++ pages/fattura/libera/libera.php
@@ -9,7 +9,7 @@
         <div class="flex-col col-6">
             <div class="mb-1 card-body">
                 <label for="head" class="form-label">Intestazione</label>
-                <textarea  class="form-control" id="head" name="head" rows="8"><?php echo _clean("
+                <textarea  class="form-control" name="head" rows="8"><?php echo _clean("
                         Daniela Zanotti
                         Dr. in Fisioterapia
                         Specialista in Terapia Manuale
@@ -24,7 +24,7 @@
         <div class="flex-col col-3">
             <div class="mb-1 card-body">
                 <label for="head" class="form-label">Dati</label>
-                <textarea  class="form-control" id="head" name="head" rows="8"><?php echo _clean("
+                <textarea  class="form-control" name="dati" rows="8"><?php echo _clean("
                     Spett.le
                     Aprea Ettore
                     via F. Gaelota 23
@@ -38,11 +38,11 @@
         <div class="flex-col col-3">
             <div class="mb-1 card-body pb-4">
                 <label for="head" class="form-label">Numero e data</label>
-                <textarea  class="form-control" id="head" name="head" rows="1"><?php echo _clean("Fattura 415 del 03/10/2024");?></textarea>
+                <textarea  class="form-control" name="date" rows="1"><?php echo _clean("Fattura 415 del 03/10/2024");?></textarea>
             </div>
             <div class="mb-1 card-body py-0">
                 <label for="head" class="form-label">Piè di pagina</label>
-                <textarea  class="form-control" id="head" name="head" rows="4"><?php echo _clean("
+                <textarea  class="form-control" name="footer" rows="4"><?php echo _clean("
                     P.IVA: 06191421210
                     C.F.ZNT DNL 64P58 F839W
                     VIA LEOPARDI N.253
@@ -51,23 +51,53 @@
             </div>
         </div>
     </div>
+    <div class="d-flex flex-col card mx-3">
+        <div class="card-body d-flex flex-row pb-0">
+            <div class="flex-col col-6">
+                <div class="card-body pe-1 pb-0">
+                    <label class="form-label">OGGETTO</label>
+                    <input  class="form-control" />
+                </div>
+                <div class="card-body pe-1 pb-0 pt-1"><input  class="form-control" value="Bollo"/></div>
+                <div class="card-body pe-1 pb-0 pt-1"><input  class="form-control" value="IMPONIBILE" disabled/></div>
+            </div>
+            <div class="flex-col col-6 ms-0">
+                <div class="card-body ps-0 pb-0">
+                    <label class="form-label">IMPORTI</label>
+                    <input type="number" class="form-control"/>
+                </div>
+                <div class="card-body ps-0 pb-0 pt-1"><input type="number" class="form-control" value="2.00"/></div>
+                <div class="card-body ps-0 pb-0 pt-1"><input type="number" class="form-control" value="2.00" disabled/></div>
+            </div>
+        </div>
+        <div class="flex-fill px-4 mx-2 pb-2 pt-3"><button class="btn btn-primary w-100">AGGIUNGI RIGA</button></div>
+        <hr class="my-1">
+        <div class="card-body d-flex flex-row">
+            <div class="flex-col col-6">
+                <div class="card-body pe-1 pb-0 pt-1"><input  class="form-control fs-5" value="TOTALE FATTURA" disabled/></div>
+            </div>
+            <div class="flex-col col-6 ms-0">
+                <div class="card-body ps-0 pb-0 pt-1"><input type="number" class="form-control fs-5" value="2.00" disabled/></div>
+            </div>
+        </div>
+    </div>  
     <div class="d-flex flex-row">
-        <div class="flex-col col-11">
-            <div class="mb-1 card-body pt-0">
+        <div class="flex-col col-12">
+            <div class="mb-1 card-body ">
                 <label for="head" class="form-label">Articolo</label>
-                <textarea  class="form-control" id="head" name="head" rows="1"><?php echo _clean("Operazione esente da Iva effettuata ai sensi dell'art. 10, DPR 633/72");?></textarea>
+                <textarea  class="form-control" name="articolo" rows="1"><?php echo _clean("Operazione esente da Iva effettuata ai sensi dell'art. 10, DPR 633/72");?></textarea>
             </div>
         </div>
-        <div class="flex-col col-1" onclick="generatePDF();">
-            <div class="mb-1 card-body pt-4 px-0 mt-2">
-                <button class="btn btn-primary" id="print"><a class="me-2"><?php echo icon('print.svg','white',20,20) ?></a>Genera</button>
-            </div>
-        </div>
+    </div>
+    <div class="flex-col" onclick="generatePDF();">
+        <div class="flex-fill px-3"><button class="btn btn-primary w-100"><a class="me-2"><?php echo icon('print.svg','white',20,20) ?></a>Genera</button></div>
     </div>
 </div>
 <script>
     function generatePDF() {
-        $.post('post/fattura_libera.php').done(response => {
+        let _data = {};
+        document.querySelectorAll('textarea[name]').forEach(element =>{ _data[element.getAttribute('name')]=element.textContent });
+        $.post('post/fattura_libera.php',_data).done(response => {
             const link = document.createElement('a');
             link.href = response; 
             link.target = '_blank'; 
--- post/fattura_libera.php
+++ post/fattura_libera.php
@@ -3,8 +3,29 @@
 
     $pdf = new FPDF();
     $pdf->AddPage();
-    $pdf->SetFont('Arial', 'B', 16);
-    $pdf->Cell(40, 10, "TEST");
+    $pdf->SetFont('Arial', 'B', 10);
+    $pdf->MultiCell(0, 6, trim($_REQUEST['head']));
+    $pdf->Ln();
+    $width=0;
+    foreach (explode("\n",trim($_REQUEST['dati'])) as $value) {
+        $current=$pdf->GetStringWidth($value);
+        $width = $current>$width?$current:$width;
+    }
+    $pdf->SetX($pdf->GetPageWidth() - $width - 20);
+    $pdf->MultiCell($width +10, 6, trim($_REQUEST['dati']), 0, 'C');
+    $pdf->Ln();
+    $pdf->SetFont('Arial', 'B', 14);
+    $pdf->MultiCell(0, 6, trim($_REQUEST['date']));
+    $pdf->Ln();
+    $pdf->SetFont('Arial', 'B', 6);
+    $pdf->MultiCell(0, 6, trim($_REQUEST['articolo']));
+    $pdf->Ln();
+
+    
+    $pdf->SetY(-40);
+    $pdf->MultiCell(0, 4, trim($_REQUEST['footer']),0,'C');
+    
+    
     $file="arquive/fatture/".datetime().'.pdf';
     $pdf->Output('F', root($file));
     echo url($file);
