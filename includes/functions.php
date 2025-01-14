9b5ebe3b6d810d616df13e1667d39950abf3c452
= <=>
2025-01-14 17:36:43 +0100

    inp

--- includes/functions.php
+++ includes/functions.php
@@ -87,10 +87,7 @@
         echo '<script component="modal_'.$component.'" src="'.$full_path.'?v='.filemtime($full_path).'"></script>';
     }
     function root(string $path):string{
-        switch(environment()){
-            case 'dev':return $_SERVER['DOCUMENT_ROOT'].root_path($path);
-            case 'prod':return $_SERVER['DOCUMENT_ROOT'] . '/' . ltrim($path, '/');
-        }
+        return $_SERVER['DOCUMENT_ROOT'].root_path($path);
     }
     function theme():string{
         return Session()->get('template')->theme ?? 'blue';
