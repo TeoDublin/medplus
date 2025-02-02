42d4c927cf8dfa682dce8237df7ce09b4284573a
= <teodublin@gmail.com>
2025-02-02 20:40:11 +0100

    inp

--- components/search_table/search_table.php
+++ components/search_table/search_table.php
@@ -30,8 +30,8 @@
                                 </th>";
                         }
                         foreach ($_REQUEST['actions']??[] as $key=>$value){
-                            $left=substr($key,0,3);
-                            $right=substr($key,4,strlen($key)-3);
+                            $left=substr($key,0,1);
+                            $right=substr($key,2,strlen($key)-1);
                             echo "<th scope=\"col\" class=\"text-center\" rowspan=\"2\"><span class=\"me-1\">{$left}</span><span class=\"d-none d-md-table-cell\">{$right}</span></th>";   
                         }?>
                         <th scope="col" class="text-center" rowspan="2">#</th>
