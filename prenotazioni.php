351e6cdb0110698a4ea0cda65410d45125edff55
= <=>
2025-02-09 18:48:33 +0100

    inp

--- prenotazioni.php
+++ prenotazioni.php
@@ -1,6 +1,6 @@
 <?php 
 require_once 'includes.php';
-require_once 'includes/session.php';
+
 require_once 'pages/prenotazioni/ctrl.php';
 require_once 'includes/header.php';
 require_once 'includes/menu-header.php';
