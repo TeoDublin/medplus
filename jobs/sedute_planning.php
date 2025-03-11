14816bdec299d30870c38541062f7d461c6d7867
teo <teodublin@gmail.com>
2025-03-11 20:00:30 +0100

    inp

--- jobs/sedute_planning.php
+++ jobs/sedute_planning.php
@@ -1,12 +1,22 @@
 <?php 
     require_once __DIR__.'/../includes.php';
     $now=now('Y-m-d H:m:i');
-    $prenotati=Select('p.*')
+    $sedute=Select('p.*')
         ->from('view_planning','p')
-        ->where("p.origin IN('Seduta')")
+        ->where("p.origin = 'seduta'")
         ->and("p.data_fine < '{$now}'")
         ->and("p.stato = 'Prenotata'")
         ->get();
-    foreach($prenotati as $prenotato){
+    foreach($sedute as $prenotato){
         Update('percorsi_terapeutici_sedute_prenotate')->set(['stato_prenotazione'=>'Conclusa'])->where("id={$prenotato['id']}")->flush();
+    }
+
+    $colloquio=Select('p.*')
+        ->from('view_planning','p')
+        ->where("p.origin = 'colloquio'")
+        ->and("p.data_fine < '{$now}'")
+        ->and("p.stato = 'Prenotata'")
+        ->get();
+    foreach($sedute as $prenotato){
+        Update('colloquio_planning')->set(['stato_prenotazione'=>'Conclusa'])->where("id={$prenotato['id']}")->flush();
     }
