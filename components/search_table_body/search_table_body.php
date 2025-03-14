935c5a4dc09f2c2f8d3c881da8e9a97920530199
teo <teodublin@gmail.com>
2025-03-14 21:37:14 +0100

    inp

--- components/search_table_body/search_table_body.php
+++ components/search_table_body/search_table_body.php
@@ -1,7 +1,7 @@
 <?php
     $response=json_decode($_REQUEST['response']);
     $is_first=true;
-    if($response->total==0)echo "<div class=\"text-center mt-3\"><h3>Non Trovato</h3></div>";
+    if($response->total==0)echo "<div class=\"text-center text-wrap mt-3\"><h3>Non Trovato</h3></div>";
     else{
         foreach ($response->result  as $row) {
             $is_first=true;?>
@@ -12,32 +12,32 @@
                         $tr_class='';
                     }
                     else $tr_class='d-none d-md-table-cell';
-                    if($col!='id')echo "<td scope=\"col\" class=\"text-center {$tr_class}\">{$row->{$col}}</td>";
+                    if($col!='id')echo "<td scope=\"col\" class=\"text-center text-wrap {$tr_class}\">{$row->{$col}}</td>";
                 }
                 if($response->actions){
                     foreach (json_decode($response->actions) as $key => $value) {
                         switch ($value) {
                             case 'success':{
-                                echo "<td scope=\"col\" class=\"text-center hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success')\";>".icon('heart.svg')."</td>";
+                                echo "<td scope=\"col\" class=\"text-center text-wrap hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success')\";>".icon('heart.svg')."</td>";
                                 break;
                             }
                             case 'success2':{
-                                echo "<td scope=\"col\" class=\"text-center hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success2')\";>".icon('people-arms.svg')."</td>";
+                                echo "<td scope=\"col\" class=\"text-center text-wrap hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success2')\";>".icon('people-arms.svg')."</td>";
                                 break;
                             }
                             case 'success3':{
-                                echo "<td scope=\"col\" class=\"text-center hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success3')\";>".icon('dollar.svg')."</td>";
+                                echo "<td scope=\"col\" class=\"text-center text-wrap hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success3')\";>".icon('dollar.svg')."</td>";
                                 break;
                             }
                             case 'success4':{
-                                echo "<td scope=\"col\" class=\"text-center hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success4')\";>".icon('document.svg')."</td>";
+                                echo "<td scope=\"col\" class=\"text-center text-wrap hover-icon\" title=\"{$key}\" onmouseenter=\"hoverIconAdd(this,'success4')\";>".icon('document.svg')."</td>";
                                 break;
                             }
                         }
                     }   
                 }
                 if(in_array('btn_delete_search_table',Session()->get('elementi')??[])){?>
-                    <td scope="col" class="text-center action-Elimina" title="Elimina" 
+                    <td scope="col" class="text-center text-wrap action-Elimina" title="Elimina" 
                         onmouseenter="hoverIconWarning(this)";><?php echo icon('bin.svg');?>
                     </td><?php
                 }?>
