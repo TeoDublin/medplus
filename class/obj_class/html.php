6d7d1777d32505672af0315f25a7aef7d6619d5f
teo <teodublin@gmail.com>
2025-03-14 21:59:45 +0100

    inp

--- class/obj_class/html.php
+++ class/obj_class/html.php
@@ -10,9 +10,9 @@ class Html{
         if (!str_contains($url, '?')) $url .= '?';
         if ($result->pages > 1 && !$_REQUEST['search']) { ?>
             <div class="d-flex align-content-end">
-                <div class="ms-auto flex-grow-0 me-3">
+                <div class="ms-auto flex-grow-0 me-md-3">
                     <nav aria-label="Page navigation example">
-                        <ul class="pagination">
+                        <ul class="pagination ">
                             <li class="page-item">
                                 <a class="page-link" href="<?php echo $url . 'pagination=' . ($this->offset() == 0 ? 0 : $this->offset() - 1); ?>" aria-label="indietro">
                                     <span>&laquo;</span>
@@ -26,11 +26,11 @@ class Html{
                                     $active = ($i == $current) ? 'active' : '';
                                     echo '<li class="page-item ' . $active . '"><a class="page-link" href="' . $url . 'pagination=' . $i . '">' . ($i + 1) . '</a></li>';
                                 }
-                                echo '<li class="page-item disabled"><span class="page-link">...</span></li>';
+                                echo '<li class="page-item disabled"><span class="page-link"><span class="d-block d-md-none">.</span><span class="d-none d-md-block">...</span></span></li>';
                                 if($current>=3&&$current<($total_pages - 2)){
                                     $active = 'active';
                                     echo '<li class="page-item ' . $active . '"><a class="page-link" href="' . $url . 'pagination=' . $current . '">' . ($current +1). '</a></li>';
-                                    echo '<li class="page-item disabled"><span class="page-link">...</span></li>';
+                                    echo '<li class="page-item disabled"><span class="page-link"><span class="d-block d-md-none">.</span><span class="d-none d-md-block">...</span></span></li>';
                                 }
                                 for ($i = $total_pages - 2; $i <= $total_pages; $i++) {
                                     $active = ($i == $current) ? 'active' : '';
