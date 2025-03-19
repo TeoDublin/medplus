7f9f5150bbebe3b780612decbb57d2c1dce74473
TeoDublin <78739500+TeoDublin@users.noreply.github.com>
2025-03-19 11:28:34 +0100

    Update medplus.sql

--- migrate/medplus.sql
+++ migrate/medplus.sql
@@ -314,3 +314,88 @@ ALTER TABLE `utenti_preferenze`
 ALTER TABLE `utenti_preferenze`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
+
+DROP VIEW view_planning;
+CREATE VIEW view_planning AS
+SELECT 
+  'sbarra' AS `origin`,
+  `pm`.`id` AS `id`,
+  `pm`.`row_inizio` AS `row_inizio`,
+  `pm`.`row_fine` AS `row_fine`,
+  `pm`.`row_fine` - `pm`.`row_inizio` + 1 AS `row_span`,
+  `pm`.`id_terapista` AS `id_terapista`,
+  `pm`.`data` AS `data`,
+  CONCAT(`pm`.`data`, ' ', `pr`.`ora`) AS `data_fine`,
+  `m`.`motivo` AS `motivo`,
+  '-' AS `stato`
+FROM 
+  `planning_motivi` `pm`
+  LEFT JOIN `motivi` `m` ON `pm`.`id_motivo` = `m`.`id`
+  LEFT JOIN `planning_row` `pr` ON `pm`.`row_fine` = `pr`.`id`
+
+UNION ALL
+
+SELECT 
+  'seduta' AS `origin`,
+  `sp`.`id` AS `id`,
+  `sp`.`row_inizio` AS `row_inizio`,
+  `sp`.`row_fine` AS `row_fine`,
+  `sp`.`row_fine` - `sp`.`row_inizio` + 1 AS `row_span`,
+  `sp`.`id_terapista` AS `id_terapista`,
+  `sp`.`data` AS `data`,
+  CONCAT(`sp`.`data`, ' ', `pr`.`ora`) AS `data_fine`,
+  CONCAT(`c`.`nominativo`, ' (', `t`.`trattamenti`, ')') AS `motivo`,
+  `sp`.`stato_prenotazione` AS `stato`
+FROM 
+  `percorsi_terapeutici_sedute_prenotate` `sp`
+  LEFT JOIN `planning_row` `pr` ON `sp`.`row_fine` = `pr`.`id`
+  LEFT JOIN `clienti` `c` ON `sp`.`id_cliente` = `c`.`id`
+  LEFT JOIN `percorsi_terapeutici_sedute` `s` ON `sp`.`id_seduta` = `s`.`id`
+  LEFT JOIN `percorsi_combo` `pc` ON `s`.`id_combo` = `pc`.`id`
+  LEFT JOIN 
+    (SELECT 
+       `pct`.`id_combo` AS `id_combo`,
+       GROUP_CONCAT(`t`.`trattamento` SEPARATOR ', ') AS `trattamenti`
+     FROM 
+       `percorsi_combo_trattamenti` `pct`
+       LEFT JOIN `trattamenti` `t` ON `pct`.`id_trattamento` = `t`.`id`
+     GROUP BY `pct`.`id_combo`) `t` 
+  ON `pc`.`id` = `t`.`id_combo`
+WHERE 
+  `sp`.`stato_prenotazione` <> 'Assente'
+
+UNION ALL
+
+SELECT 
+  'corso' AS `origin`,
+  `cp`.`id` AS `id`,
+  `cp`.`row_inizio` AS `row_inizio`,
+  `cp`.`row_fine` AS `row_fine`,
+  `cp`.`row_fine` - `cp`.`row_inizio` + 1 AS `row_span`,
+  `cp`.`id_terapista` AS `id_terapista`,
+  `cp`.`data` AS `data`,
+  CONCAT(`cp`.`data`, ' ', `pr`.`ora`) AS `data_fine`,
+  CONCAT(`cp`.`motivo`,' ( Corso )') AS `motivo`,
+  '-' AS `stato`
+FROM 
+  `corsi_planning` `cp`
+  LEFT JOIN `planning_row` `pr` ON `cp`.`row_fine` = `pr`.`id`
+
+UNION ALL
+
+SELECT 
+  'colloquio' AS `origin`,
+  `cp`.`id` AS `id`,
+  `cp`.`row_inizio` AS `row_inizio`,
+  `cp`.`row_fine` AS `row_fine`,
+  `cp`.`row_fine` - `cp`.`row_inizio` + 1 AS `row_span`,
+  `cp`.`id_terapista` AS `id_terapista`,
+  `cp`.`data` AS `data`,
+  CONCAT(`cp`.`data`, ' ', `pr`.`ora`) AS `data_fine`,
+  CONCAT(`c`.`nominativo`, ' ( Colloquio )') AS `motivo`,
+  `cp`.`stato_prenotazione` AS `stato`
+FROM 
+  `colloquio_planning` `cp`
+  LEFT JOIN `clienti` `c` ON `cp`.`id_cliente` = `c`.`id`
+  LEFT JOIN `planning_row` `pr` ON `cp`.`row_fine` = `pr`.`id`
+
