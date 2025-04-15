2e4d87dda1556283b4e7e6f2e0294cfe13bb72c4
teo <teodublin@gmail.com>
2025-04-15 18:15:06 +0000

    stato fattura change

--- post/fattura_cambia_stato.php
+++ post/fattura_cambia_stato.php
@@ -2,12 +2,12 @@
     $_REQUEST['skip_cookie']=true;
     require_once '../includes.php';
     function _percorsi(){
-        return Select('f.data,pf.id_percorso')
+        return Select('f.data,pf.id_origine')
             ->from('pagamenti_fatture','pf')
             ->left_join('fatture f ON pf.id_fattura = f.id')
             ->where("pf.id_fattura={$_REQUEST['id']}")->get();
     }
     Update('fatture')->set(['stato'=>$_REQUEST['stato']])->where("id={$_REQUEST['id']}");
     foreach (_percorsi() as $percorso) {
-        if($percorso['origine']!='corsi')Sedute()->refresh($percorso['id_percorso'],$percorso['data'],'Fattura');
+        if($percorso['origine']!='corsi')Sedute()->refresh($percorso['id_origine'],$percorso['data'],'Fattura');
     }
