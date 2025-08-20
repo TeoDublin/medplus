2123a5b166ea64b960faf0045b875ed44f05e6e4
teo <teodublin@gmail.com>
2025-08-20 13:40:05 +0000

    inp

--- post/dont_remember_password.php
+++ post/dont_remember_password.php
@@ -8,6 +8,8 @@
     else{
         $token=bin2hex(random_bytes(6));
         Update('utenti')->set(['token'=>$token,'expiry'=>add_date('+1 hour')])->where("id={$user['id']}");
-        $sent=_Mail()->send($user['email'],'Cambia Password',"clica sul link per cambiare la password: <a href='https://teo.place/medplus/change_password.php?id={$user['id']}&token=$token'>https://teo.place/medplus/change_password.php</a>");
+        $href=url("change_password.php?id={$user['id']}&token={$token}");
+        $a=url("change_password.php");
+        $sent=_Mail()->send($user['email'],'Cambia Password',"clica sul link per cambiare la password: <a href='{$href}'>{$a}</a>");
         echo $sent?'mail_sent':'error_sending';
     }
